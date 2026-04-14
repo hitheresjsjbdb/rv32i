571b1948dd6a1c9caa0ed36bac1eb70d5a7c9b1b
Jiaming Luo <simly0440@gmail.com>
2026-04-14 11:55:54 +0800

    load hex file

--- cpu/semu/include/mem/mem.h
+++ cpu/semu/include/mem/mem.h
@@ -5,3 +5,4 @@
 word_t memRead(size_t addr, size_t length);
 void memWrite(size_t addr, word_t data, size_t length);
 word_t instRead(size_t addr);
+bool loadMemFromHex(char * filename);
--- cpu/semu/src/semu-main.cpp
+++ cpu/semu/src/semu-main.cpp
@@ -1,8 +1,10 @@
 #include "cpu/init.h"
 #include "sdb/sdb.h"
 #include "run/run.h"
+#include "mem/mem.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc > 1) loadMemFromHex(argv[1]);
     sim::waveInit("wave.vcd");
     init();
     sdbLoop();
