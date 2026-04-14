0d1fb933e848794f564bd63a818e166fc560c3b7
Jiaming Luo <simly0440@gmail.com>
2026-04-14 12:06:58 +0800

    fixed some bugs

--- cpu/semu/src/diff/diff.cpp
+++ cpu/semu/src/diff/diff.cpp
@@ -11,7 +11,7 @@ static bool checkReg() {
     for (int i {}; i < 32; i++) {
         word_t dutReg = sim::getReg(i);
         if (dutReg != R(i)) {
-            printf("Reg check failed: DUT's reg[%d] = %d, while SEMU's reg[%d] = %d\n", i, dutReg, i, R(i));
+            printf("Reg check failed: DUT's reg[%d] = 0x%08x, while SEMU's reg[%d] = 0x%08x\n", i, dutReg, i, R(i));
             success = false;
         }
     }
@@ -21,7 +21,7 @@ static bool checkReg() {
 static bool checkPC() {
     word_t dutPC = sim::getPC();
     if (cpu.pc != dutPC) {
-        printf("PC check failed: DUT's pc = %d, while SEMU's pc = %d\n", dutPC, cpu.pc);
+        printf("PC  check failed: DUT's pc = 0x%08x, while SEMU's pc = 0x%08x\n", dutPC, cpu.pc);
         return false;
     }
     return true;
